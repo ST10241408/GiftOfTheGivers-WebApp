f880bd29a30ea2c430288b67340ad5c615a7ea7d
Phathisa Ndaliso <ST10241408@rcconnect.edu.za>
2025-09-18 09:15:13 +0200

    Clean project files and add .gitignore

---
+++ ST10241408_GiftOfTheGiversWebApp/Models/Money.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ST10241408_GiftOfTheGiversWebApp.Models
+{
+    public class Money
+    {
+        [Key]
+        public int MoneyId { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal TotalMoney { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal RemainingMoney { get; set; }
+    }
+}
